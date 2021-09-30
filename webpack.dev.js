9726e9c98efa001fea3304903ad2b6706bb2db3d
Andrey Poznyak <andpoznyak@gmail.com>
2021-09-30 13:11:36 +0300

    configuring the project

---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: "[path][name].[ext]?hash=[hash:20]",
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: true,
+    }),
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,88 @@
+const path = require('path');
+
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const buildPath = path.resolve(__dirname, 'docs');
+
+module.exports = {
+    entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: buildPath,
+    clean: true,
+  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+              },
+            {
+                // Load all images as base64 encoding if they are smaller than 8192 bytes
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name].[hash:20].[ext]',
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            // Inject the js bundle at the end of the body of the given template
+            //inject: 'body',
+        }),
+        new FaviconsWebpackPlugin({
+            // Your source logo
+            logo: './assets/icon.png',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'favicons/',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+            title: 'Peech App',
+
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css'
+        }),
+        // new OptimizeCssAssetsPlugin({
+        //     cssProcessor: require('cssnano'),
+        //     cssProcessorOptions: {
+        //         map: {
+        //             inline: false,
+        //         },
+        //         discardComments: {
+        //             removeAll: true
+        //         }
+        //     },
+        //     canPrint: true
+        // })
+    ]
+};
