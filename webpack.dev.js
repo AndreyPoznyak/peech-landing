5b8099bddf627289cedfe062691c9728d76cf3e7
Andrey Poznyak <andpoznyak@gmail.com>
2021-10-01 12:23:27 +0300

    added the logo

--- webpack.dev.js
+++ webpack.dev.js
@@ -20,20 +20,10 @@ module.exports = {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
           },
-      {
-        // Load all images as base64 encoding if they are smaller than 8192 bytes
-        test: /\.(png|jpg|gif)$/,
-        use: [
           {
-            loader: "url-loader",
-            options: {
-              // On development we want to see where the file is coming from, hence we preserve the [path]
-              name: "[path][name].[ext]?hash=[hash:20]",
-              limit: 8192,
-            },
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
           },
-        ],
-      },
     ],
   },
   plugins: [
