dbb5d4d4a4e1a173c4a116869e78261768a67925
Andrey Poznyak <andpoznyak@gmail.com>
2021-10-19 13:30:54 +0300

    added pixel event + fixed the href

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const link = document.querySelector('.store-link');
     const params = new URLSearchParams(location.search.substring(1));
     const trackerId = params.get('tracker') || 'x0myqi4'; //TODO: make sure param name is relevant
-    const trackerUrl = `https://app.adjust.com/${trackerId}?${location.search}`;
+    const trackerUrl = `https://app.adjust.com/${trackerId}${location.search}`;
 
     link.href = trackerUrl;
+
+    link.addEventListener('click', () => {
+        const pixelCall = fbq || (() => console.log('No Pixel initted.'));
+        pixelCall('track', 'CompleteRegistration');
+    });
 });
