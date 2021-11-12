d7505ca2667fa4182900a4a659354178e5c24285
Andrey Poznyak <andpoznyak@gmail.com>
2021-11-12 23:19:20 +0200

    added ViewContent event

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
     const link = document.querySelector('.store-link');
-    const params = new URLSearchParams(location.search.substring(1));
-    //const trackerId = params.get('tracker') || 'x0myqi4';
+    const pixelCall = fbq || (() => console.log('No Pixel initted.'));
     const trackerUrl = `https://app.adjust.com/x0myqi4${location.search || '?campaign=Landing'}`;
+
+    pixelCall('track', 'ViewContent'); //to have the same event as SM
 
     link.href = trackerUrl;
 
     link.addEventListener('click', () => {
-        const pixelCall = fbq || (() => console.log('No Pixel initted.'));
         pixelCall('track', 'CompleteRegistration');
     });
 });
