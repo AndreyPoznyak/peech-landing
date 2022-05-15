26d37ef9e84fb8cecc2bb00367e63a3bd9688363
Andrey Poznyak <andpoznyak@gmail.com>
2022-05-16 00:10:51 +0300

    changes again

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -18,7 +18,7 @@ module.exports = Merge(CommonConfig, {
         host: 'localhost',
         port: 3000,
         proxy: 'http://localhost:8080',
-        files: ['docs', '_src'],
+        files: ['_site', '_src'],
       },
       {
         reload: false,
@@ -29,7 +29,7 @@ module.exports = Merge(CommonConfig, {
   module: {},
   devServer: {
     static: {
-      directory: path.resolve('docs'),
+      directory: path.resolve('_site'),
     },
     hot: true,
     client: {
