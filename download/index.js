918af5a73e37ed1c4e70b6b0c1b4c28eaad8ccb7
Andrey Poznyak <andpoznyak@gmail.com>
2022-07-14 01:22:36 +0300

    improved url generation, cookie check for fb added

--- download/index.js
+++ download/index.js
@@ -1,48 +1,35 @@
 import './index.scss';
-import './onelink-smart-script-latest.js';
+
+const getCookieValue = (name) => document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '';
 
 document.addEventListener('DOMContentLoaded', () => {
     const oneLinkURL = "https://peech.onelink.me/mjNH/";
     const linkEl = document.querySelector('.store-link');
+    const urlParams = new URLSearchParams(location.search);
+    const facebookClickId = getCookieValue('_fbc') || urlParams.get('fbclid') || '';
+    const facebookBrowserId = getCookieValue('_fbp') || '';
+    const afParams = {
+        pid: urlParams.get('utm_source') || 'no_media_source',
+        c: urlParams.get('utm_campaign'),
+        af_c_id: urlParams.get('campaign_id'),
+        af_adset_id: urlParams.get('adset_id'),
+        af_ad_id: urlParams.get('ad_id'),
+        af_channel: urlParams.get('utm_medium'),
+        af_ad: urlParams.get('utm_term'),
+        af_adset: urlParams.get('utm_content'),
+        af_sub1: facebookClickId,
+        af_sub2: facebookBrowserId,
+        af_sub3: urlParams.get('placement'),
+        af_sub4: urlParams.get('gclid'), //google click
+    };
 
-    if (!AF_SMART_SCRIPT) {
-        console.log('OneLink script is not loaded.');
-        linkEl.href = oneLinkURL;
-        return;
-    }
+    const queryString = Object.keys(afParams)
+        .filter(key => !!afParams[key])
+        .map(key => `${key}=${afParams[key]}`).join('&');
 
-    const afSub1 = { keys: ['fbclid'], defaultValue: '' };
-    const afSub2 = { keys: ['gclid'], defaultValue: '' };
-    const afSub3 = { keys: ['placement'], defaultValue: '' };
-    const mediaSource = { keys: ["utm_source"], defaultValue: 'no_media_source' };
-    const channel = {keys: ["utm_medium", "utm_channel"]};
-    const campaign = {keys: ["campaign", "utm_campaign"]};
-    const ad = {keys: ["utm_term", "utm_ad"]};
-    const adSet = {keys: ["utm_content", "utm_adset", "utm_adset_name"]};
-    const campaignId = {keys: ["campaign_id", "utm_campaign_id"]};
-    const adsetId = {keys: ["adset_id"]};
-    const adId = {keys: ["ad_id"]};
-
-    const { clickURL } = AF_SMART_SCRIPT.generateOneLinkURL({
-        oneLinkURL,
-        afParameters: {
-            afSub1,
-            afSub2,
-            afSub3,
-            mediaSource,
-            channel,
-            campaign,
-            ad,
-            adSet,
-            campaignId,
-            adsetId,
-            adId,
-        }
-    });
+    const clickURL = `${oneLinkURL}?${queryString}`;
 
     console.log(clickURL);
 
     linkEl.href = clickURL;
 });
-
-//?utm_source=aaa&utm_medium=cpc&utm_campaign={{campaign.name}}&utm_content={{adset.name}}&utm_term={{ad.name}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&placement={{site_source_name}}-{{placement}}
