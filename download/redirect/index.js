2609c04f5a1e39ee827e159196908a0009486ae7
Andrey Poznyak <andpoznyak@gmail.com>
2022-10-28 17:11:31 +0200

    handling copy on click at least

--- download/redirect/index.js
+++ download/redirect/index.js
@@ -5,12 +5,15 @@ import { getClickURL } from '../link';
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(location.search);
 
+    document.body.addEventListener('click', () => navigator.clipboard.writeText(`peechapp://${location.search}`)); //just in case
+
     setTimeout(() => {
         if (window.fbq) {
             fbq('track', 'StoreRedirect');
         }
-        navigator.clipboard.writeText(`peechapp://${location.search}`);
+
         window.location.href = getClickURL(urlParams);
     }, 2500);
+    
     setTranslations(urlParams.get('language'));
 });
