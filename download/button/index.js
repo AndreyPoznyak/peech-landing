b47f87f44fd5eb5a5f6cec3bd61384ac48122782
Andrey Poznyak <andpoznyak@gmail.com>
2022-10-28 14:56:50 +0200

    handling click anywhere

--- download/button/index.js
+++ download/button/index.js
@@ -5,7 +5,10 @@ import { getClickURL } from '../link';
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(location.search);
     const linkEl = document.querySelector('.store-link');
-    
-    linkEl.href = getClickURL(urlParams);
+    const link = getClickURL(urlParams);
+
     setTranslations(urlParams.get('language'));
+
+    linkEl.href = link;
+    document.body.addEventListener('click', () => window.location.href = link); 
 });
