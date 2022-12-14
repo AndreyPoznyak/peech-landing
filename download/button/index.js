4e1b794be277c4f3cb8d322c9c868346abbd940b
Andrey Poznyak <andpoznyak@gmail.com>
2022-12-15 00:20:29 +0200

    added delay before redirect to make sure calls go through

--- download/button/index.js
+++ download/button/index.js
@@ -2,6 +2,19 @@ import './index.scss';
 import { setTranslations } from './l10n';
 import { getClickURL } from '../link';
 
+const goToStore = (link) => {
+    setTimeout(() => window.location.href = link, 200);
+
+    if (window.fbq) {
+        fbq('track', 'ViewContent');
+    }
+    if (window.gtag) {
+        gtag('event', 'StoreRedirect');
+    }
+    
+    navigator.clipboard.writeText(`peechapp://${location.search}`);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(location.search);
     const linkEl = document.querySelector('.store-link');
@@ -11,15 +24,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     linkEl.href = link;
 
-    document.body.addEventListener('click', () => {
-        if (window.fbq) {
-            fbq('track', 'ViewContent');
-        }
-        if (window.gtag) {
-            gtag('event', 'StoreRedirect');
-        }
-        
-        navigator.clipboard.writeText(`peechapp://${location.search}`);
-        window.location.href = link;
+    linkEl.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        goToStore(link);
     }); 
+    document.body.addEventListener('click', () => goToStore(link)); 
 });
